32741cc6638642b20b8dff1fa2bf88be341d5c1c
Barshon <barshonweb@gmail.com>
2023-11-12 12:04:15 +0600

    header and footer set

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../Pages/Shared/Header';
+import Footer from '../Pages/Shared/Footer';
 
 const Main = () => {
     return (
-        <div>
+        <div className='max-w-7xl mx-auto'>
+            <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
