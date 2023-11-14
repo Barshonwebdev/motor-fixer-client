69a280db6c6c5d219c49eacba66a1c079633da91
Barshon <barshonweb@gmail.com>
2023-11-14 22:22:10 +0600

    authentication implemented

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import  { useContext } from "react";
 import loginImg from "../../assets/images/login/login.svg";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 const Register = () => {
+
+    const {createUser}=useContext(AuthContext);
+
   const handleSignUp = (event) => {
     event.preventDefault();
+    const form=event.target;
+    const email=form.email.value;
+    const password=form.password.value;
+
+    createUser(email,password)
+    .then(result=>{
+        const user=result.user;
+        console.log(user);
+    })
   };
   return (
     <div className="hero  ">
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,18 +1,39 @@
-import { Children, createContext, useState } from "react";
-import {getAuth} from "firebase/auth"
+import { createContext, useEffect, useState } from "react";
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword} from "firebase/auth"
 import app from "../Firebase/firebase.config";
 const auth=getAuth(app);
 export const AuthContext = createContext();
-const AuthProvider = ({Children}) => {
-    const [user,setUser]=useState();
+const AuthProvider = ({children}) => {
+    const [user,setUser]=useState(null);
     const [loading,setLoading]=useState(true);
+
+    const createUser=(email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+    const signIn=(email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    useEffect(()=>{
+        const unsubscribe=onAuthStateChanged(auth,currentUser=>{
+            setUser(currentUser);
+            console.log("currentUser: ", currentUser);
+            setLoading(false);
+        });
+
+        return ()=>{
+            return unsubscribe();
+        }
+    },[])
     const authInfo={
-        user,loading
+        user,loading,createUser,signIn
     };
    
     return (
         <AuthContext.Provider value={authInfo}>
-            {Children}
+            {children}
         </AuthContext.Provider>
     );
 };
